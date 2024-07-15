d84c6825e1a92182ef482df43361bd1f7c06a7fa
vinay546221 <154580847+vinay546221@users.noreply.github.com>
2024-07-15 15:33:55 +0530

    terraform.tf

--- project.java
+++ project.java
@@ -1,2 +1,41 @@
-hello good morning
+provider "aws" {
+  region     = "ap-southeast-1"
+}
+resource "aws_security_group" "web_access" {
+        name = "web_access"
+        description = "allow ssh and http"
+
+        ingress {
+                from_port = 80
+                to_port = 80
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        ingress {
+                from_port = 22
+                to_port = 22
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+        egress {
+                from_port = 0
+                to_port = 0
+                protocol = "-1"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+
+
+}
+resource "aws_instance" "example" {
+  ami           = "ami-0e97ea97a2f374e3d"
+  instance_type = "t2.micro"
+  key_name      = "terra"
+  security_groups = ["${aws_security_group.web_access.name}"]
+
+  tags = {
+    Name = "tf-example"
+  }
+}
 
